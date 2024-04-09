d57abade5d6639c7976cdac158d40792c9f5972e
aiva0071 <166111416+aiva0071@users.noreply.github.com>
2024-04-09 16:37:34 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -420,8 +420,8 @@ def main():
 
         
             file_paths1 = r'10YearDatabase.csv'
-            file_paths2 = r'call-report-data-2023-12\FOICU.txt'
-            file_paths3 = r'Real Data Updated last 2 april\ATM Locations.csv'
+            file_paths2 = r'FOICU.txt'
+            file_paths3 = r'ATM Locations.csv'
             # df, df2 = load_data_from_db(file_path1,file_path2)
             df = pd.read_csv(file_paths1)
             df2 = pd.read_csv(file_paths2)
